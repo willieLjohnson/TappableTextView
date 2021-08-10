d81da9cdce2434d49dc87a702093954aea0bec21
SlickJohnson <liwa.johnson@gmail.com>
2021-08-09 20:05:51 -0400

    Bug fix

--- Helper/Style.swift
+++ Helper/Style.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 /// Holds the style guide of the app
-struct Style {
+public struct Style {
   static let accentColor: UIColor = #colorLiteral(red: 0.3450980392, green: 0.337254902, blue: 0.8392156863, alpha: 1)
   static let contentColor: UIColor = #colorLiteral(red: 0.3490196078, green: 0.3450980392, blue: 0.4509803922, alpha: 1)
   static let shadowColor: CGColor = #colorLiteral(red: 0.3490196078, green: 0.3450980392, blue: 0.4509803922, alpha: 0.5)
